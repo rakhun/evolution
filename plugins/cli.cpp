e095551d8b643d9ad6d220c8e0eebe455b4456ef
Rakhun (Henrik Nilsson) <henrik30000@gmail.com>
2008-12-22 21:14:44 +0100

    Parse commands into argv and argc variables for easier handling, implemented a create command

--- plugins/cli.cpp
+++ plugins/cli.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include "../pointers.h"
+#include "../creature.h"
 
 void* commandinput(void* pointer)
 {
@@ -16,15 +17,52 @@ void* commandinput(void* pointer)
     fgets(buffer, 1024, stdin);
     int tmp=strlen(buffer)-1;
     if(buffer[tmp]=='\n') buffer[tmp]=0;
-    //if(!strncmp(buffer, "command ", 8))
-    if(!strcmp(buffer, "exit"))
+    int argc=1;
+    for(unsigned int i=0; i<strlen(buffer); i++)
+    {
+      if(buffer[i]==' ') argc++;
+    }
+    char** argv=new char*[argc];
+    char* arg=buffer;
+    unsigned int i=0;
+    while(*arg)
+    {
+      char* tmp=strchrnul(arg, ' ');
+      tmp[0]=0;
+      argv[i]=new char[tmp-arg+1];
+      strcpy(argv[i], arg);
+      arg=tmp+1;
+      i++;
+    }
+    /// @todo Delete this mem when you're done
+    if(!strcmp(argv[0], "exit"))
     {
       exit(0);
     }
-    else if(!strcmp(buffer, "help"))
+    else if(!strcmp(argv[0], "help"))
     {
       puts("Available commands:");
-      puts("exit         Exits EvolutionBots");
+      puts("exit                  Exits EvolutionBots");
+      puts("create [type] [X] [Y] Creates a new creature at X, Y.");
+      puts("                       Type=default or manual (using a default");
+      puts("                       COL script or entering code manually)");
+    }
+    else if(!strcmp(argv[0], "create"))
+    {
+      if(argc!=4)
+      {
+        puts("Usage: create [type=default/manual] [X] [Y]");
+        continue;
+      }
+      if(!strcmp("manual", argv[1]))
+      {
+        puts("Manual creation not yet implemented.");
+      }else{ // Default to 'default'
+        /// @todo Write some proper default code that actually does something.
+        creature* baby=new creature(atoi(argv[2]), atoi(argv[3]), (const unsigned char*)"");
+        std::vector<creature*>* people=(std::vector<creature*>*)((pointers*)pointer)->getPointer("creatures");
+        people->push_back(baby);
+      }
     }
     else{puts("Unknown command. Type 'help' to get a list of available commands");}
   }
