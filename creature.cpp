5012c282c860c4e3fbafc0deb405a2b0d874a456
Rakhun <henrik30000@gmail.com>
2008-12-26 20:33:03 +0100

    Licensing the project under GPLv3

--- creature.cpp
+++ creature.cpp
@@ -1,3 +1,13 @@
+/*
+ * Evolution - An experiment with evolving scripts through natural selection
+ * Copyright (C) 2008 Henrik Nilsson
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses>.
+ */
 /// @file creature.cpp
 /// This file holds most of the creature class' functionality, except for COL execution which resides in col.cpp
 /// @brief Main code for the creature class
